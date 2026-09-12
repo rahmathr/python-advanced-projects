772e8e2d5cf7832fb126eb6b35bf3019c52d0963
Rahmat Hidayat Ramadhan <rahmathr.king@gmail.com>
2026-09-12 13:45:21 +0700

    feat: add search and filtering (v1.2.0)

--- 01-personal-productivity-manager/main.py
+++ 01-personal-productivity-manager/main.py
@@ -139,6 +139,7 @@ def cari_tugas():
                     print(f'Judul: {tugas["judul_tugas"]}')
                     print(f'Kategori: {tugas["kategori_tugas"]}')
                     print(f'Prioritas: {tugas["prioritas_tugas"]}')
+                    print(f'Status: {tugas["status_tugas"]}')
                     tugas_ditemukan = True
             if not tugas_ditemukan:
                 print("\nKata kunci tidak ditemukan!")
@@ -146,6 +147,223 @@ def cari_tugas():
             break
         else:
             print("\nError: Kata kunci tidak boleh kosong!")
+            continue
+
+def filter_tugas():
+    while True:
+        print("\nFilter Tugas")
+        print("1. Berdasarkan Kategori")
+        print("2. Berdasarkan Prioritas")
+        print("3. Berdasarkan Status")
+        pilihan_filter = input("\nPilih filter: ")
+        if pilihan_filter != "":
+            if pilihan_filter.isdigit():
+                if int(pilihan_filter) == 1:
+                    kategori_tugas = pilih_kategori()
+                    tugas_ditemukan = False
+                    for tugas in daftar_tugas:
+                        if kategori_tugas == tugas["kategori_tugas"]:
+                            print("")
+                            print(f'ID-{tugas["id"]}')
+                            print(f'Judul: {tugas["judul_tugas"]}')
+                            print(f'Kategori: {tugas["kategori_tugas"]}')
+                            print(f'Prioritas: {tugas["prioritas_tugas"]}')
+                            print(f'Status: {tugas["status_tugas"]}')
+                            tugas_ditemukan = True
+                    if not tugas_ditemukan:
+                        print("\nKategori tidak ditemukan!")
+                        continue
+                    break
+                elif int(pilihan_filter) == 2:
+                    prioritas_tugas = pilih_prioritas()
+                    tugas_ditemukan = False
+                    for tugas in daftar_tugas:
+                        if prioritas_tugas == tugas["prioritas_tugas"]:
+                            print("")
+                            print(f'ID-{tugas["id"]}')
+                            print(f'Judul: {tugas["judul_tugas"]}')
+                            print(f'Kategori: {tugas["kategori_tugas"]}')
+                            print(f'Prioritas: {tugas["prioritas_tugas"]}')
+                            print(f'Status: {tugas["status_tugas"]}')
+                            tugas_ditemukan = True
+                    if not tugas_ditemukan:
+                        print("\nPrioritas tidak ditemukan!")
+                        continue
+                    break
+                elif int(pilihan_filter) == 3:
+                    print("\n1. Belum selesai")
+                    print("2. Selesai")
+                    pilihan_status = input("\nPilih status: ").strip()
+                    if pilihan_status != "":
+                        if pilihan_status.isdigit():
+                            if int(pilihan_status) == 1:
+                                tugas_ditemukan = False
+                                for tugas in daftar_tugas:
+                                    if "Belum selesai" == tugas["status_tugas"]:
+                                        print("")
+                                        print(f'ID-{tugas["id"]}')
+                                        print(f'Judul: {tugas["judul_tugas"]}')
+                                        print(f'Kategori: {tugas["kategori_tugas"]}')
+                                        print(f'Prioritas: {tugas["prioritas_tugas"]}')
+                                        print(f'Status: {tugas["status_tugas"]}')
+                                        tugas_ditemukan = True
+                                if not tugas_ditemukan:
+                                    print("\nStatus tidak ditemukan!")
+                                    continue
+                                break
+                            elif int(pilihan_status) == 2:
+                                tugas_ditemukan = False
+                                for tugas in daftar_tugas:
+                                    if "Selesai" == tugas["status_tugas"]:
+                                        print("")
+                                        print(f'ID-{tugas["id"]}')
+                                        print(f'Judul: {tugas["judul_tugas"]}')
+                                        print(f'Kategori: {tugas["kategori_tugas"]}')
+                                        print(f'Prioritas: {tugas["prioritas_tugas"]}')
+                                        print(f'Status: {tugas["status_tugas"]}')
+                                        tugas_ditemukan = True
+                                if not tugas_ditemukan:
+                                    print("\nStatus tidak ditemukan!")
+                                    continue
+                                break
+                            else:
+                                print("\nError: Pilihan status tidak tersedia! Pilih hanya angka 1 dan 2.")
+                                continue
+                        else:
+                            print("\nError: Pilihan status harus berupa angka, tidak boleh huruf atau simbol!")
+                            continue
+                    else:
+                        print("\nError: Pilihan status tidak boleh kosong!")
+                        continue
+                else:
+                    print("\nError: Pilihan filter tidak tersedia! Pilih hanya angka 1, 2, atau 3.")
+                    continue
+            else:
+                print("\nError: Pilihan filter harus berupa angka, tidak boleh huruf atau simbol!")
+                continue
+        else:
+            print("\nError: Pilihan filter tidak boleh kosong!")
+            continue
+
+def sort_tugas():
+    while True:
+        print("\nSort Tugas")
+        print("1. Berdasarkan Judul")
+        print("2. Berdasarkan Prioritas")
+        print("3. Berdasarkan Kategori")
+        pilihan_sort = input("\nPilih sort: ")
+        if pilihan_sort != "":
+            if pilihan_sort.isdigit():
+                if int(pilihan_sort) == 1:
+                    print("\nJenis Sort")
+                    print("1. A -> Z")
+                    print("2. Z -> A")
+                    pilihan_jenis_sort = input("\nPilih jenis sort: ")
+                    if pilihan_jenis_sort != "":
+                        if pilihan_jenis_sort.isdigit():
+                            if int(pilihan_jenis_sort) == 1:
+                                tugas_urut_judul_a_ke_z = sorted(daftar_tugas, key=lambda x: x["judul_tugas"])
+                                for tugas in tugas_urut_judul_a_ke_z:
+                                    print(f'\nID-{tugas["id"]}')
+                                    print(f'Judul: {tugas["judul_tugas"]}')
+                                    print(f'Kategori: {tugas["kategori_tugas"]}')
+                                    print(f'Prioritas: {tugas["prioritas_tugas"]}')
+                                    print(f'Status: {tugas["status_tugas"]}')
+                                break
+                            elif int(pilihan_jenis_sort) == 2:
+                                tugas_urut_judul_z_ke_a = sorted(daftar_tugas, key=lambda x: x["judul_tugas"], reverse=True)
+                                for tugas in tugas_urut_judul_z_ke_a:
+                                    print(f'\nID-{tugas["id"]}')
+                                    print(f'Judul: {tugas["judul_tugas"]}')
+                                    print(f'Kategori: {tugas["kategori_tugas"]}')
+                                    print(f'Prioritas: {tugas["prioritas_tugas"]}')
+                                    print(f'Status: {tugas["status_tugas"]}')
+                                break
+                            else:
+                                print("\nError: Pilihan sort tidak tersedia! Pilih hanya angka 1 dan 2.")
+                                continue
+                        else:
+                            print("\nError: Pilihan sort harus berupa angka, tidak boleh huruf atau simbol!")
+                            continue
+                    else:
+                        print("\nError: Pilihan sort tidak boleh kosong!")
+                        continue
+
+                elif int(pilihan_sort) == 2:
+                    print("\nJenis Sort")
+                    print("1. Tinggi -> Rendah")
+                    print("2. Rendah -> Tinggi")
+                    bobot_prioritas = {"Tinggi": 1, "Sedang": 2, "Rendah": 3}
+                    pilihan_jenis_sort = input("\nPilih jenis sort: ")
+                    if pilihan_jenis_sort != "":
+                        if pilihan_jenis_sort.isdigit():
+                            if int(pilihan_jenis_sort) == 1:
+                                tugas_urut_prioritas_tinggi_ke_rendah = sorted(daftar_tugas, key=lambda x: bobot_prioritas[x["prioritas_tugas"]])
+                                for tugas in tugas_urut_prioritas_tinggi_ke_rendah:
+                                    print(f'\nID-{tugas["id"]}')
+                                    print(f'Judul: {tugas["judul_tugas"]}')
+                                    print(f'Kategori: {tugas["kategori_tugas"]}')
+                                    print(f'Prioritas: {tugas["prioritas_tugas"]}')
+                                    print(f'Status: {tugas["status_tugas"]}')
+                                break
+                            elif int(pilihan_jenis_sort) == 2:
+                                tugas_urut_prioritas_rendah_ke_tinggi = sorted(daftar_tugas, key=lambda x: bobot_prioritas[x["prioritas_tugas"]], reverse=True)
+                                for tugas in tugas_urut_prioritas_rendah_ke_tinggi:
+                                    print(f'\nID-{tugas["id"]}')
+                                    print(f'Judul: {tugas["judul_tugas"]}')
+                                    print(f'Kategori: {tugas["kategori_tugas"]}')
+                                    print(f'Prioritas: {tugas["prioritas_tugas"]}')
+                                    print(f'Status: {tugas["status_tugas"]}')
+                                break
+                            else:
+                                print("\nError: Pilihan sort tidak tersedia! Pilih hanya angka 1 dan 2.")
+                                continue
+                        else:
+                            print("\nError: Pilihan sort harus berupa angka, tidak boleh huruf atau simbol!")
+                            continue
+                    else:
+                        print("\nError: Pilihan sort tidak boleh kosong!")
+                        continue
+
+                elif int(pilihan_sort) == 3:
+                    print("\nJenis Sort")
+                    print("1. A -> Z")
+                    print("2. Z -> A")
+                    pilihan_jenis_sort = input("\nPilih jenis sort: ")
+                    if pilihan_jenis_sort != "":
+                        if pilihan_jenis_sort.isdigit():
+                            if int(pilihan_jenis_sort) == 1:
+                                tugas_urut_kategori_a_ke_z = sorted(daftar_tugas, key=lambda x: x["kategori_tugas"])
+                                for tugas in tugas_urut_kategori_a_ke_z:
+                                    print(f'\nID-{tugas["id"]}')
+                                    print(f'Judul: {tugas["judul_tugas"]}')
+                                    print(f'Kategori: {tugas["kategori_tugas"]}')
+                                    print(f'Prioritas: {tugas["prioritas_tugas"]}')
+                                    print(f'Status: {tugas["status_tugas"]}')
+                                break
+                            elif int(pilihan_jenis_sort) == 2:
+                                tugas_urut_kategori_z_ke_a = sorted(daftar_tugas, key=lambda x: x["kategori_tugas"], reverse=True)
+                                for tugas in tugas_urut_kategori_z_ke_a:
+                                    print(f'\nID-{tugas["id"]}')
+                                    print(f'Judul: {tugas["judul_tugas"]}')
+                                    print(f'Kategori: {tugas["kategori_tugas"]}')
+                                    print(f'Prioritas: {tugas["prioritas_tugas"]}')
+                                    print(f'Status: {tugas["status_tugas"]}')
+                                break
+                            else:
+                                print("\nError: Pilihan sort tidak tersedia! Pilih hanya angka 1 dan 2.")
+                                continue
+                        else:
+                            print("\nError: Pilihan sort harus berupa angka, tidak boleh huruf atau simbol!")
+                            continue
+                    else:
+                        print("\nError: Pilihan sort tidak boleh kosong!")
+                        continue
+            else:
+                print("\nError: Pilihan sort harus berupa angka, tidak boleh huruf atau simbol!")
+                continue
+        else:
+            print("\nError: Pilihan Sort tidak boleh kosong!")
             continue
 
 def tandai_selesai():
@@ -212,9 +430,11 @@ while True:
     2. Tampilkan tugas
     3. Hapus tugas
     4. Cari tugas
-    5. Tandai tugas selesai
-    6. Edit tugas
-    0. Keluar
+    5. Filter tugas
+    6. Sort tugas
+    7. Tandai tugas selesai
+    8. Edit tugas
+    9. Keluar
     ''')
     pilihan_menu = input("Pilih menu: ").strip()
     if pilihan_menu.isdigit():
@@ -227,10 +447,14 @@ while True:
         elif int(pilihan_menu) == 4:
             cari_tugas()
         elif int(pilihan_menu) == 5:
-            tandai_selesai()
+            filter_tugas()
         elif int(pilihan_menu) == 6:
+            sort_tugas()
+        elif int(pilihan_menu) == 7:
+            tandai_selesai()
+        elif int(pilihan_menu) == 8:
             edit_tugas()
-        elif int(pilihan_menu) == 0:
+        elif int(pilihan_menu) == 9:
             break
         else:
             print("\nError: Pilihan menu tidak tersedia!")
